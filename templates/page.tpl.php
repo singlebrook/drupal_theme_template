a18081616ce99df42e59c1f8b7039cff870c7f5c
Alex <alatzl@gmail.com>
2013-04-30 10:27:22 -0400

    Converts templates to use HTML5 elements.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,42 +1,42 @@
 <div id="page-wrapper"><div id="page">
 
-  <div id="header"><div class="section clearfix">
+  <header role="banner">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"><strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong></div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
+      <?php if ($site_name || $site_slogan): ?>
+        <div id="name-and-slogan">
+          <?php if ($site_name): ?>
+            <?php if ($title): ?>
+              <div id="site-name"><strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </strong></div>
+            <?php else: /* Use h1 when the content title is empty */ ?>
+              <h1 id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
           <?php endif; ?>
-        <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?>
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php endif; ?>
+        </div> <!-- /#name-and-slogan -->
+      <?php endif; ?>
 
-    <?php print render($page['header']); ?>
+      <?php print render($page['header']); ?>
 
-  </div></div> <!-- /.section, /#header -->
+  </header> <!-- / header -->
 
   <?php if ($main_menu || $secondary_menu): ?>
-    <div id="navigation"><div class="section">
+    <nav role="navigation" id="navigation">
       <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
       <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-    </div></div> <!-- /.section, /#navigation -->
+    </nav> <!-- / nav -->
   <?php endif; ?>
 
   <?php if ($breadcrumb): ?>
@@ -45,7 +45,7 @@
 
   <?php print $messages; ?>
 
-  <div id="main-wrapper"><div id="main" class="clearfix">
+  <div id="main" class="clearfix" role="main">
 
     <div id="content" class="column"><div class="section">
       <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
@@ -72,10 +72,12 @@
       </div></div> <!-- /.section, /#sidebar-second -->
     <?php endif; ?>
 
-  </div></div> <!-- /#main, /#main-wrapper -->
+  </div> <!-- /#main -->
 
-  <div id="footer"><div class="section">
-    <?php print render($page['footer']); ?>
-  </div></div> <!-- /.section, /#footer -->
+  <footer role="contentinfo">
+    <div class="section">
+      <?php print render($page['footer']); ?>
+    </div>  <!-- /.section -->
+  </footer> <!-- / footer -->
 
 </div></div> <!-- /#page, /#page-wrapper -->
